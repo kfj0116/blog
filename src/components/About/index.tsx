eec3edad11ddb67f96165c6d92a515f9c5373b47
kfj0116 <1104172465@qq.com>
2020-09-10 22:35:10 +0300

    Add marked and highlight.js

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import marked from 'marked';
+import hljs from 'highlight.js';
+
+import 'highlight.js/styles/monokai-sublime.css';
 import './style.scss';
 
 const About: React.FC = () => {
-  return <div className="blog-about">This is me.</div>;
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    highlight: function (code) {
+      return hljs.highlightAuto(code).value;
+    },
+  });
+  const content = '# This is me.';
+  return (
+    <div
+      className="blog-about"
+      dangerouslySetInnerHTML={{
+        __html: marked(content),
+      }}
+    ></div>
+  );
 };
 
 export default About;
